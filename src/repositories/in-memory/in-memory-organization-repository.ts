b67d60940bc889e840c7f0932b9adcde1c16778b
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-01-07 14:36:22 -0300

    feat: basic organizations in memory repository.

---
+++ src/repositories/in-memory/in-memory-organization-repository.ts
@@ -0,0 +1,35 @@
+import { Organization, Prisma } from '@prisma/client';
+import { randomUUID } from 'crypto';
+
+import { OrganizationRepository } from '../@types';
+
+export class InMemoryOrganizationRepository implements OrganizationRepository {
+  private orgs: Organization[] = [];
+
+  async create(data: Prisma.OrganizationCreateInput): Promise<Organization> {
+    const org: Organization = {
+      id: data.id ? data.id : randomUUID(),
+      address: data.address,
+      cep: data.cep,
+      created_at: new Date(),
+      email: data.email,
+      name: data.name,
+      password_hash: data.password_hash,
+      whatsapp_phone: data.whatsapp_phone
+    };
+
+    this.orgs.push(org);
+
+    return org;
+  }
+
+  async findById(organization_id: string): Promise<Organization | null> {
+    const org = this.orgs.find(el => el.id === organization_id);
+
+    if (!org) {
+      return null;
+    }
+
+    return org;
+  }
+}
